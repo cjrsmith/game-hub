c93fa2f236b1f57e4206a0668d67e4ca042b6017
Connor J.R. Smith <connorsmith5111@googlemail.com>
2023-08-26 14:23:56 +0100

    Sort the games

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -23,7 +23,11 @@ const useGames = (gameQuery: GameQuery) =>
   useData<Game>(
     "/games",
     {
-      params: { genres: gameQuery.genre?.id, platform: gameQuery.platform?.id },
+      params: {
+        genres: gameQuery.genre?.id,
+        platform: gameQuery.platform?.id,
+        ordering: gameQuery.sortOrder,
+      },
     },
     [gameQuery]
   );
