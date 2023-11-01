7b11282ed53917d7ca0466179be140485f5f12f1
Connor J.R. Smith <csmith@oxfordquantumcircuits.com>
2023-11-01 16:31:36 +0000

    Refactoring- fetch games with react query

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,13 @@
 import axios from "axios";
 
+export interface FetchResponse<T> {
+  count: number;
+  results: T[];
+}
+
 export default axios.create({
-    baseURL: 'https://api.rawg.io/api',
-    params: {
-        key: '4ccbcea79657465ca08bf981e37d6d4c'
-    }
-})
+  baseURL: "https://api.rawg.io/api",
+  params: {
+    key: "4ccbcea79657465ca08bf981e37d6d4c",
+  },
+});
